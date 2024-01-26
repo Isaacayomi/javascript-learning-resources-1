4b17461afd508ee411d05afea22ab8d6d2f99a7c
Isaac Ayomide <okunlolaa3@outlook.com>
2024-01-26 09:11:23 +0100

    completed coding challenge 3

--- Bankist App/script.js
+++ Bankist App/script.js
@@ -268,14 +268,12 @@ console.log(movements);
 // calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 
 //CODING CHALLENGE 3 (rewriting the solution of the previous challenge using arrow function)
-const calcAverageHumanAge = function (ages) {
-  const humanAge =
-    ages
-      .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
-      .filter(age => age >= 18)
-      .reduce((acc, age, i, arr) => {
-        acc + age;
-      }, 0) / arr.length;
-  console.log(humanAge);
-};
-calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => 
+      acc + age / arr.length, 0);
+// console.log(humanAge);
+const ave1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+console.log(ave1);
