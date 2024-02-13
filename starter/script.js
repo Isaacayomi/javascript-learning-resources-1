48344cc57c760f625772d68e6438a34ab95d54f1
Isaac Ayomide <okunlolaa3@outlook.com>
2024-02-13 14:03:11 +0100

    revised insertAdjacentElement

--- starter/script.js
+++ starter/script.js
@@ -56,8 +56,10 @@ const message = document.createElement('div');
 message.classList.add('cookie-message');
 message.innerHTML = `Use cookies for improved functionality and analytics <button class="btn btn-close-cookie">Got it! </button>`;
 
+header.insertAdjacentElement("beforebegin", message); // beforebegin, afterbegin, beforened, afterend
+
 //To insert the newly created elements in the DOM
-header.prepend(message); //inserts the newly created element as the first child of the header element
+// header.prepend(message); //inserts the newly created element as the first child of the header element
 
 // header.append(message) //adds the created element as the last child of the header element
 
@@ -65,8 +67,8 @@ header.prepend(message); //inserts the newly created element as the first child 
 // header.after(message); //Adds the new element after the header element
 
 // Deleting Elements
-document
-  .querySelector('.btn-close-cookie')
-  .addEventListener('click', function () {
-    message.remove();
-  });
+// document
+//   .querySelector('.btn-close-cookie')
+//   .addEventListener('click', function () {
+//     message.remove();
+//   });
