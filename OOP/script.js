919592ead895e0e1db35c476095286beba3f56f5
Isaac Ayomide <okunlolaa3@outlook.com>
2024-05-29 16:06:02 +0100

    learning prototypal inheritance

--- OOP/script.js
+++ OOP/script.js
@@ -15,7 +15,7 @@ const Person = function (firstName, birthYear) {
 
 // The above constructor function now acts as the blueprint of a house, and with the blueprint set, other houses can be created based on the existing blueprint. Just like the new objects (matilda and jack) created below.
 
-const Isaac = new Person("Isaac", 2003); // The actual house created
+const Isaac = new Person("Isaac", 2003); // The actual house created (constructor function)
 console.log(Isaac);
 
 // 1. New empty object () is created
@@ -25,8 +25,16 @@ console.log(Isaac);
 
 // Instances created from the above class Person.
 const matilda = new Person("Maltida", 2017);
-const jack = new Person("Jack", 2019);
-const prime = new Person("Prime", 1990);
-console.log(prime)
-console.log(matilda);
-console.log(jack);
+// const jack = new Person("Jack", 2019);
+// const prime = new Person("Prime", 1990);
+// console.log(prime);
+// console.log(matilda);
+// console.log(jack);
+
+// PROTOTYPES
+// Each and every function in javascript automatically has a property called prototype, and that includes constructor functions. Instead of adding methods directly in to the constructor function, we use prototypes.
+Person.prototype.calcAge = function () {
+  console.log(2024 - this.birthYear);
+};
+
+Isaac.calcAge();
